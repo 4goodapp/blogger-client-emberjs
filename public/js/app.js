beaa6a67c246fe0b7f201ba78da2713804394213
@NAME@ <@EMAIL@>
2013-10-01 20:04:37 +0800

    Added helpers markdown and moment

--- public/js/app.js
+++ public/js/app.js
@@ -32,18 +32,28 @@ App.PostController = Ember.ObjectController.extend({
     }
 });
 
+Ember.Handlebars.helper('format-date', function(date) {
+    return moment(date).fromNow();
+});
+
+Ember.Handlebars.helper('format-markdown', function(md) {
+    return new Handlebars.SafeString(new Showdown.converter().makeHtml(md));
+});
+
+Ember.Handlebars
+
 var posts = [{
     id: '1',
     title: 'test one two three',
     author: { name: 'Hank' },
-    date: new Date(),
-    excerpt: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi earum possimus quidem animi perferendis esse eaque reprehenderit quis soluta suscipit. Ipsum, fugiat, laboriosam dolores nobis nihil iste perspiciatis suscipit quibusdam.",
-    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, facilis, dolores, et cupiditate illum quasi deleniti consequatur maiores minima possimus porro repellendus voluptatibus quam. Nihil, voluptates consequatur aut odit consequuntur!"
+    date: new Date() - 60 * 60 * 2 * 1000,
+    excerpt: "# Lorem ipsum dolor sit amet \n consectetur adipisicing elit. Quasi earum possimus quidem animi perferendis esse eaque reprehenderit quis soluta suscipit. Ipsum, fugiat, laboriosam dolores nobis nihil iste perspiciatis suscipit quibusdam.",
+    body: "## Lorem ipsum dolor sit amet \n consectetur adipisicing elit. Nam, facilis, dolores, et cupiditate illum quasi deleniti consequatur maiores minima possimus porro repellendus voluptatibus quam. Nihil, voluptates consequatur aut odit consequuntur!"
 }, {
     id: '2',
     title: 'This is test 2',
     author: { name: 'Hank' },
-    date: new Date(),
-    excerpt: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi earum possimus quidem animi perferendis esse eaque reprehenderit quis soluta suscipit. Ipsum, fugiat, laboriosam dolores nobis nihil iste perspiciatis suscipit quibusdam.",
-    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, facilis, dolores, et cupiditate illum quasi deleniti consequatur maiores minima possimus porro repellendus voluptatibus quam. Nihil, voluptates consequatur aut odit consequuntur!"
+    date: new Date() - 60 * 60 * 4 * 1000,
+    excerpt: "# Lorem ipsum dolor sit amet \n consectetur adipisicing elit. Quasi earum possimus quidem animi perferendis esse eaque reprehenderit quis soluta suscipit. Ipsum, fugiat, laboriosam dolores nobis nihil iste perspiciatis suscipit quibusdam.",
+    body: "## Lorem ipsum dolor sit amet \n consectetur adipisicing elit. Nam, facilis, dolores, et cupiditate illum quasi deleniti consequatur maiores minima possimus porro repellendus voluptatibus quam. Nihil, voluptates consequatur aut odit consequuntur!"
 }];
